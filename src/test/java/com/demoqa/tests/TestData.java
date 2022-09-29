4f1da1d9740bcc8720dd4c4325594d109dc45cad
noredcat <anastasiaredcat@gmail.com>
2022-09-30 00:10:45 +0700

    add JavaFaker

--- src/test/java/com/demoqa/tests/TestData.java
+++ src/test/java/com/demoqa/tests/TestData.java
@@ -1,8 +1,11 @@
 package com.demoqa.tests;
 
+import com.github.javafaker.Faker;
+
 import static java.lang.String.format;
 public class TestData {
-   static String firstName = "Ivan",
+   Faker faker = new Faker();
+   static String firstName = Faker.instance().name().firstName(),
             lastName = "Ivanov",
             expectedFullName = format("%s %s", firstName, lastName);
 
