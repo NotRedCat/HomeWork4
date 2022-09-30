c8dfbd9adc25d327d34fd7888c24d88d753aeafe
noredcat <anastasiaredcat@gmail.com>
2022-09-30 20:51:28 +0700

    used variables

--- src/test/java/com/demoqa/tests/TestData.java
+++ src/test/java/com/demoqa/tests/TestData.java
@@ -3,10 +3,23 @@ package com.demoqa.tests;
 import com.github.javafaker.Faker;
 
 import static java.lang.String.format;
+
 public class TestData {
-   Faker faker = new Faker();
-   static String firstName = Faker.instance().name().firstName(),
-            lastName = "Ivanov",
-            expectedFullName = format("%s %s", firstName, lastName);
+    Faker faker = new Faker();
+
+    static String firstName = Faker.instance().name().firstName(),
+            lastName = Faker.instance().name().lastName(),
+            expectedFullName = format("%s %s", firstName, lastName),
+            email = Faker.instance().internet().emailAddress(),
+            gender = "Male",
+            number = "7" + Faker.instance().number().digits(9),
+            month = "April",
+            day = Faker.instance().number().numberBetween(10, 30) + "",
+            year = Faker.instance().number().numberBetween(1980, 2007) + "",
+            subject = "Computer Science",
+            hobby = "Music",
+            file = "it-cat.jpg",
+            address = Faker.instance().address().fullAddress(), state = "NCR", city = "Gurgaon";
+
 
 }
--- src/test/java/com/demoqa/utils/RandomUtils.java
+++ src/test/java/com/demoqa/utils/RandomUtils.java
@@ -1,5 +1,11 @@
 package com.demoqa.utils;
 
+import com.github.javafaker.service.RandomService;
+
+import java.security.SecureRandom;
+import java.util.*;
+import java.util.random.RandomGenerator;
+
 public class RandomUtils {
 
 }
