80d4a1ae8cbceae15a4268c1826312c4d96208a5
noredcat <anastasiaredcat@gmail.com>
2022-10-10 23:00:31 +0700

    fixed

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -2,19 +2,11 @@ package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.demoqa.tests.properties.SystemPropertiesTest;
-import helpers.Attach;
+import com.demoqa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.util.Map;
 
 import static java.lang.String.format;
 
