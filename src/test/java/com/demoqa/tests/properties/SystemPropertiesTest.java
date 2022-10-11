9a820e562e50a387f1fb5581549fd012b73f219e
noredcat <anastasiaredcat@gmail.com>
2022-10-11 21:13:52 +0700

    fixed

--- src/test/java/com/demoqa/tests/properties/SystemPropertiesTest.java
+++ src/test/java/com/demoqa/tests/properties/SystemPropertiesTest.java
@@ -3,6 +3,8 @@ package com.demoqa.tests.properties;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
 public class SystemPropertiesTest {
     String browserName,
             browserVersion,
@@ -10,14 +12,16 @@ public class SystemPropertiesTest {
 
     @Test
     @Tag("properties_test")
-    void simplePropertyTest1() {
-        browserName = System.getProperty("browser_name", "firefox");
-        browserVersion = System.getProperty("browser_version", "101");
-        browserSize = System.getProperty("browser_size", "1920x1080");
 
-        System.out.println(browserName);
+    void simplePropertyTest1() {
+        step("Open registration form", () ->
+        {browserName = System.getProperty("browser_name", "firefox");
+        browserVersion = System.getProperty("browser_version", "101");
+        browserSize = System.getProperty("browser_size", "1920x1080");});
+        step("Open registration form", () ->
+        {System.out.println(browserName);
         System.out.println(browserVersion);
-        System.out.println(browserSize);
+        System.out.println(browserSize);});
     }
 
 }
